8e00cec2036afba3e27df5e8749783367679fe09
Mikolaj Babicz <koding252@gmail.com>
2026-03-26 17:40:08 +0100

    feat- modified homedashboard so it's much better
    - edited homedashboard changing the display, colors that depend if the lessen is completed/upcoming/canceled
    - added number of students displayed with colors, changed when server appears
    - added LessonStatuses for mock data

--- yourney/src/components/DashboardHome.tsx
+++ yourney/src/components/DashboardHome.tsx
@@ -1,8 +1,8 @@
-import { Box, Button, Divider, List, ListItem, ListItemText, Paper, Typography } from '@mui/material';
+import { Box, Button, Paper, Typography } from '@mui/material';
 import { useState } from 'react';
 import { PopUpComponent } from './reusableComponents/Popup';
-import { mockLessons } from '../mocks/lessonMock';
-
+import { mockLessons, type LessonStatus } from '../mocks/lessonMock';
+import AssignmentIcon from '@mui/icons-material/Assignment';
 import AccessTimeIcon from '@mui/icons-material/AccessTime';
 import RoomIcon from '@mui/icons-material/Room';
 import GroupIcon from '@mui/icons-material/Group';
@@ -18,18 +18,17 @@ export const DashboardHome = () => {
 
   return (
     <Box sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column', p: { xs: 2, md: 4 } }}>
-      
       {popUpOpened && <PopUpComponent hidePopUp={ClosePopUp} addNote={handleAddNote} />}
 
       <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, flexGrow: 1, gap: 4 }}>
         {/* lewa kolumna 75% */}
-        <Box sx={{ width: { xs: '100%', md: '75%' }, display: 'flex', flexDirection: 'column' }}>
-          <Box sx={{ mb: 4 }}>
-            <Typography variant="h4" fontWeight="bold" >
+        <Box sx={{ width: { xs: '100%', md: '80%' }, display: 'flex', flexDirection: 'column' }}>
+          <Box sx={{ mb: 3 }}>
+            <Typography variant="h5" fontWeight="bold">
               Hello User
             </Typography>
-            <Typography variant="body1" color="text.secondary">
-              [Docelowo nazwa nauczyciela/managera/ucznia z serwera]
+            <Typography variant="body2" color="text.secondary">
+              [Docelowo zamiast user nazwa nauczyciela/managera/ucznia z serwera]
             </Typography>
           </Box>
 
@@ -37,49 +36,106 @@ export const DashboardHome = () => {
             Today's Schedule
           </Typography>
 
-          {mockLessons.map((lesson, index) => (
-            <Paper
-              key={index}
-              elevation={1}
-              sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                mb: 3,
-                borderRadius: 2,
-                overflow: 'hidden',
-                transition: 'transform 0.2s',
-                '&:hover': { transform: 'translateY(-2px)', boxShadow: 3 },
-              }}
-            >
-              <Box sx={{ p: 2.5, backgroundColor: 'primary.main' }}>
-                <Typography variant="h6" fontWeight="bold">
-                  {lesson.topic}
-                </Typography>
-                <Box sx={{ display: 'flex', alignItems: 'center', mt: 1, opacity: 0.9 }}>
-                  <RoomIcon fontSize="small" sx={{ mr: 0.5 }} />
-                  <Typography variant="body2">{lesson.room}</Typography>
-                </Box>
-              </Box>
+          {mockLessons.map((lesson, index) => {
+            // DANE NA TEMAT KOLORW Z LEKCJI Z SERWERA!
+            const getStatusColor = (status: LessonStatus) => {
+              switch (status) {
+                case 'COMPLETED':
+                  return '#10b981'; // lekcja odbyta
+                case 'UPCOMING':
+                  return '#f59e0b'; // lekcja oczekuje
+                case 'CANCELLED':
+                  return '#ef4444'; // lekcja odwolana
+                default:
+                  return '#e2e8f0'; // jak blad to szare
+              }
+            };
 
-              <Box
-                sx={{ p: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}
+            const lessonColor = getStatusColor(lesson.status);
+            return (
+              <Paper
+                key={index}
+                elevation={2} // okresla plaskosc kafelek
+                sx={{
+                  display: 'flex',
+                  flexDirection: 'column',
+                  mb: 3,
+                  borderRadius: 2,
+                  border: '1px solid #e2e8f0',
+                  borderLeft: `6px solid ${lessonColor}`,
+                  overflow: 'hidden',
+                  transition: 'transform 0.2s',
+                  '&:hover': { transform: 'translateY(-2px)', boxShadow: 3, cursor: 'pointer' },
+                }}
               >
-                <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
-                  <AccessTimeIcon fontSize="small" sx={{ mr: 1 }} />
-                  <Typography variant="body2" fontWeight="500">
-                    {lesson.startTime} - {lesson.endTime}
-                  </Typography>
+                <Box sx={{ display: 'flex', flexDirection: { xs: 'column', md: 'row' }, borderBottom: '1.5px solid #acafb3' }}>
+                  <Box sx={{ p: 2, pl: 1, maxWidth: 270 }}>
+                    <Typography fontWeight="bold">{lesson.topic}</Typography>
+                    <Box sx={{ display: 'flex', alignItems: 'center', mt: 1, opacity: 0.9 }}>
+                      <RoomIcon fontSize="small" sx={{ mr: 0.5 }} />
+                      <Typography variant="body2">{lesson.room}</Typography>
+                    </Box>
+                  </Box>
+
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      flexGrow: 1,
+                      pr:{xs:0,md: 2},
+                      pl:{xs:1,md:0},
+                      pb:{xs:1.5},
+                      justifyContent: { xs: 'flex-start', md: 'flex-end' },
+                      alignItems: 'center',
+                      gap: 1,
+                    }}
+                  >
+                  
+                    <AssignmentIcon sx={{ color: 'text.secondary' }} />
+
+                    {/* GDY SERWER TU BEDA CYFRY ZALEZNE OD TEGO JAK NAUCZYCIEL WPISAL! */}
+                    <Box sx={{ display: 'flex', width: '150px', height: '24px', borderRadius: 1, overflow: 'hidden' }}>
+                      {/* obecni */}
+                      <Box sx={{ flex: 3, backgroundColor: '#4caf50', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Typography variant="caption" fontWeight="bold" color="white">
+                          3
+                        </Typography>
+                      </Box>
+
+                      {/* spoznieni */}
+                      <Box sx={{ flex: 2, backgroundColor: '#ffb300', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Typography variant="caption" fontWeight="bold" color="white">
+                          2
+                        </Typography>
+                      </Box>
+
+                      {/* nieobecni */}
+                      <Box sx={{ flex: 1, backgroundColor: '#ef5350', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                        <Typography variant="caption" fontWeight="bold" color="white">
+                          1
+                        </Typography>
+                      </Box>
+                    </Box>
+                  </Box>
                 </Box>
 
-                <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
-                  <GroupIcon fontSize="small" sx={{ mr: 1 }} />
-                  <Typography variant="body2" fontWeight="500">
-                    Students: {lesson.studentCount}
-                  </Typography>
+                <Box sx={{ p: 2, display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+                  <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
+                    <AccessTimeIcon fontSize="small" sx={{ mr: 1 }} />
+                    <Typography variant="body2" fontWeight="500">
+                      {lesson.startTime} - {lesson.endTime}
+                    </Typography>
+                  </Box>
+
+                  <Box sx={{ display: 'flex', alignItems: 'center', color: 'text.secondary' }}>
+                    <GroupIcon fontSize="small" sx={{ mr: 1 }} />
+                    <Typography variant="body2" fontWeight="500">
+                      Students expected: {lesson.studentCount}
+                    </Typography>
+                  </Box>
                 </Box>
-              </Box>
-            </Paper>
-          ))}
+              </Paper>
+            );
+          })}
         </Box>
 
         {/* prawa kolumna 25% */}
@@ -102,22 +158,44 @@ export const DashboardHome = () => {
               Add note
             </Button>
 
-            <List disablePadding>
+            <Box
+              component="ul"
+              sx={{
+                pl: 2,
+                m: 0,
+                maxHeight: '300px',
+                overflowY: 'auto',
+
+                '&::-webkit-scrollbar': { width: '6px' },
+                '&::-webkit-scrollbar-thumb': { background: '#cbd5e1', borderRadius: '4px' },
+              }}
+            >
               {notes.length === 0 ? (
-                <Typography variant="body2" color="text.secondary" textAlign="center" sx={{ mt: 2 }}>
+                <Typography
+                  component="li"
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ mt: 2, ml: -3, listStyle: 'none', textAlign: 'center' }}
+                >
                   Your checklist is empty.
                 </Typography>
               ) : (
                 notes.map((note, index) => (
-                  <Box key={index}>
-                    <ListItem disableGutters sx={{ py: 1 }}>
-                      <ListItemText primary={note}  sx={{ wordBreak: 'break-word', m: 0 }} />
-                    </ListItem>
-                    {index < notes.length - 1 && <Divider />}
-                  </Box>
+                  <Typography
+                    component="li"
+                    variant="body2"
+                    key={index}
+                    sx={{
+                      py: 1,
+                      wordBreak: 'break-word',
+                      borderBottom: index < notes.length - 1 ? '1px solid #acafb3' : 'none',
+                    }}
+                  >
+                    {note}
+                  </Typography>
                 ))
               )}
-            </List>
+            </Box>
           </Paper>
         </Box>
       </Box>
--- yourney/src/mocks/lessonMock.ts
+++ yourney/src/mocks/lessonMock.ts
@@ -1,4 +1,3 @@
-
 export type Student = {
   id: string;
   firstName: string;
@@ -9,55 +8,61 @@ export type Lesson = {
   id: string;
   topic: string;
   room: string;
-  startTime: string; 
+  startTime: string;
   endTime: string;
   studentCount: number;
+  status: LessonStatus;
   students: Student[];
 };
+
+export type LessonStatus = 'UPCOMING' | 'COMPLETED' | 'CANCELLED';
 
 // mockowe dane lekcji
 export const mockLessons: Lesson[] = [
   {
-    id: "L-001",
-    topic: "Introduction Into Python",
-    room: "Snake-Lab (A-12)",
-    startTime: "15:30",
-    endTime: "17:00",
+    id: 'L-001',
+    topic: 'Introduction Into Python',
+    room: 'Snake-Lab (A-12)',
+    startTime: '15:30',
+    endTime: '17:00',
     studentCount: 4,
+    status: 'COMPLETED',
     students: [
-      { id: "S-101", firstName: "Jan", lastName: "Kowalski" },
-      { id: "S-102", firstName: "Zofia", lastName: "Nowak" },
-      { id: "S-103", firstName: "Kacper", lastName: "Wiśniewski" },
-      { id: "S-104", firstName: "Maja", lastName: "Wójcik" }
-    ]
+      { id: 'S-101', firstName: 'Jan', lastName: 'Kowalski' },
+      { id: 'S-102', firstName: 'Zofia', lastName: 'Nowak' },
+      { id: 'S-103', firstName: 'Kacper', lastName: 'Wiśniewski' },
+      { id: 'S-104', firstName: 'Maja', lastName: 'Wójcik' },
+    ],
   },
   {
-    id: "L-002",
-    topic: "Programming In Minecraft: Redstone Logic",
-    room: "Nether-Lab (B-05)",
-    startTime: "17:15",
-    endTime: "18:45",
+    id: 'L-002',
+    topic: 'Programming In Minecraft: Redstone Logic',
+    room: 'Nether-Lab (B-05)',
+    startTime: '17:15',
+    endTime: '18:45',
     studentCount: 6,
+    status: 'UPCOMING',
     students: [
-      { id: "S-105", firstName: "Antoni", lastName: "Kowalczyk" },
-      { id: "S-106", firstName: "Julia", lastName: "Kamińska" },
-      { id: "S-107", firstName: "Filip", lastName: "Lewandowski" },
-      { id: "S-108", firstName: "Hanna", lastName: "Zielińska" },
-      { id: "S-109", firstName: "Szymon", lastName: "Szymański" },
-      { id: "S-110", firstName: "Alicja", lastName: "Dąbrowska" }
-    ]
+      { id: 'S-105', firstName: 'Antoni', lastName: 'Kowalczyk' },
+      { id: 'S-106', firstName: 'Julia', lastName: 'Kamińska' },
+      { id: 'S-107', firstName: 'Filip', lastName: 'Lewandowski' },
+      { id: 'S-108', firstName: 'Hanna', lastName: 'Zielińska' },
+      { id: 'S-109', firstName: 'Szymon', lastName: 'Szymański' },
+      { id: 'S-110', firstName: 'Alicja', lastName: 'Dąbrowska' },
+    ],
   },
   {
-    id: "L-003",
-    topic: "Games creation in Roblox Studio",
-    room: "Roblox-Cave (C-01)",
-    startTime: "19:00",
-    endTime: "20:30",
+    id: 'L-003',
+    topic: 'Games creation in Roblox Studio',
+    room: 'Roblox-Cave (C-01)',
+    startTime: '19:00',
+    endTime: '20:30',
     studentCount: 3,
+    status: 'CANCELLED',
     students: [
-      { id: "S-111", firstName: "Oliwier", lastName: "Kozłowski" },
-      { id: "S-112", firstName: "Lena", lastName: "Jankowska" },
-      { id: "S-113", firstName: "Igor", lastName: "Mazur" }
-    ]
-  }
+      { id: 'S-111', firstName: 'Oliwier', lastName: 'Kozłowski' },
+      { id: 'S-112', firstName: 'Lena', lastName: 'Jankowska' },
+      { id: 'S-113', firstName: 'Igor', lastName: 'Mazur' },
+    ],
+  },
 ];
